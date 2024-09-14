bcb5060203aa856d171b3d4e7c50fce527f4c39e
JaywonC <jaywonc49@gmail.com>
2024-09-13 20:33:46 -0400

    Update

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 let intro = document.querySelector('.intro');
 let logoSpan = document.querySelectorAll('.logo');
 
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    logoSpan.forEach((span, idx) => {
-      setTimeout(() => {
-        span.classList.add('active');
-      }, (idx + 1) * 400);
-    });
-
+if (intro && logoSpan.length > 0) {
+  window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       logoSpan.forEach((span, idx) => {
         setTimeout(() => {
-          span.classList.remove('active');
-        }, (idx + 1) * 50);
+          span.classList.add('active');
+        }, (idx + 1) * 400);
       });
-    }, 2000);
 
-    setTimeout(() => {
-      intro.style.top = '-100vh';
-    }, 2300);
+      setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+          setTimeout(() => {
+            span.classList.remove('active');
+          }, (idx + 1) * 50);
+        });
+      }, 2000);
+
+      setTimeout(() => {
+        intro.style.top = '-100vh';
+      }, 2300);
+    }, 100);  // Adding a small initial delay just to ensure everything is rendered
   });
-});
+}
