92cb808a2bac8560e5dd5df11f390799dc2dade4
JaywonC <114188320+JaywonC@users.noreply.github.com>
2024-11-03 21:53:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ window.addEventListener("DOMContentLoaded", () => {
       logoSpan.forEach((span, idx) => {
         setTimeout(() => {
           span.classList.remove('active');
-          // span.classList.fade('fade');
         }, (idx + 1) * 50)
       })
     }, 2000);
@@ -27,24 +26,3 @@ window.addEventListener("DOMContentLoaded", () => {
 })
 
 
-
-const portfolio = document.getElementsByClassName("modal-btn")
-for (let i = 0; i < portfolio.length; i ++){
-  portfolio[i].addEventListener("click", function(){
-    const data = json[portfolio[i]["firstChild"]["id"]];
-    console.log(data);
-    document.getElementById("exampleModalLabel").innerHTML = portfolio[i]["firstChild"]["id"];
-    document.getElementById("year").innerHTML = data["year"]
-    document.getElementById("description").innerHTML = data["description"];
-    console.log($('#languages'));
-    $('#languages').innerHTML = ""
-    for (let i = 0; i < data["languages"].length; i ++){
-      console.log(data["languages"][i])
-      $('#languages').append(
-        '<div class="col-sm-3">' + 
-        '<img id="l1" class="image-fluid" src= "' + data["languages"][i] + '"/>' + 
-        '</div>'
-      )
-    }
-  })
-}
